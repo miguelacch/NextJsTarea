b140f006a295781605aa4e5b4c78eec19bb21474
Miguel <mcruzc1206@egresado.ipn.mx>
2026-04-08 02:09:50 +0000

    Manejo de imagenes y actualización de fuentes.

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'], // Especifica los pesos: normal (400) y negrita (700)
+  subsets: ['latin'],
+});
